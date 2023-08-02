3e1cb73df7fce019f3e8b0b1ffad1e832a5990f3
Reece Atkins <Reecepowered2@hotmail.com>
2023-08-02 15:30:52 -0700

    Created Book properties

---
+++ CPW211-BookRegistration/Book.cs
@@ -0,0 +1,20 @@
+﻿namespace CPW211_BookRegistration
+{
+    public class Book
+    {
+        /// <summary>
+        /// The book's ISBN number
+        /// </summary>
+        public string ISBN { get; set; }
+
+        /// <summary>
+        /// The title of the book
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The price of the book
+        /// </summary>
+        public decimal Price { get; set; }
+    }
+}
