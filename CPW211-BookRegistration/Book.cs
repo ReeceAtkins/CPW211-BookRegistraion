159658e3a6beaa18ba2f18cbea92540856c96a4a
Reece Atkins <Reecepowered2@hotmail.com>
2023-08-02 15:34:22 -0700

    Created Book Constructor

--- CPW211-BookRegistration/Book.cs
+++ CPW211-BookRegistration/Book.cs
@@ -3,6 +3,19 @@
     public class Book
     {
         /// <summary>
+        ///  Creates book with ISBN, title, and price
+        /// </summary>
+        /// <param name="isbn">The book's ISBN number</param>
+        /// <param name="title">The book's title</param>
+        /// <param name="price">The books price</param>
+        public Book(string isbn, string title, decimal price) 
+        {
+            ISBN = isbn;
+            Title = title;
+            Price = price;
+        }
+
+        /// <summary>
         /// The book's ISBN number
         /// </summary>
         public string ISBN { get; set; }
