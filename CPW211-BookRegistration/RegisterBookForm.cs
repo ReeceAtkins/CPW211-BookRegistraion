82cbb6f5c113a0b6fd1da244b5c6a8768bdae1fc
Reece Atkins <Reecepowered2@hotmail.com>
2023-08-22 15:49:46 -0700

    Populated Customers combo box

--- CPW211-BookRegistration/RegisterBookForm.cs
+++ CPW211-BookRegistration/RegisterBookForm.cs
@@ -6,5 +6,23 @@ namespace CPW211_BookRegistration
         {
             InitializeComponent();
         }
+
+        private void RegisterBookForm_Load(object sender, EventArgs e)
+        {
+            PopulateCustomer();
+        }
+
+        private void PopulateCustomer()
+        {
+            // Clear any previous objects
+            cboCustomers.Items.Clear();
+
+            List<Customer> customers = CustomerDB.GetAllCustomers();
+
+            foreach (Customer currentCustomer in customers)
+            {
+                cboCustomers.Items.Add(currentCustomer);
+            }
+        }
     }
 }
