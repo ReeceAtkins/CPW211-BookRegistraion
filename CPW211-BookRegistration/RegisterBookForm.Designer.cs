2643c9fc5d1951eedeaa9a62b651ee79388f3337
Reece Atkins <Reecepowered2@hotmail.com>
2023-08-30 17:16:02 -0700

    Created Delete form

--- CPW211-BookRegistration/RegisterBookForm.Designer.cs
+++ CPW211-BookRegistration/RegisterBookForm.Designer.cs
@@ -41,14 +41,14 @@
             lblBookError = new Label();
             lblDateError = new Label();
             lblErrorMsg = new Label();
+            btnDelete = new Button();
             SuspendLayout();
             // 
             // btnAddCustomer
             // 
-            btnAddCustomer.Location = new Point(10, 9);
-            btnAddCustomer.Margin = new Padding(3, 2, 3, 2);
+            btnAddCustomer.Location = new Point(11, 12);
             btnAddCustomer.Name = "btnAddCustomer";
-            btnAddCustomer.Size = new Size(156, 63);
+            btnAddCustomer.Size = new Size(178, 84);
             btnAddCustomer.TabIndex = 4;
             btnAddCustomer.Text = "Add Customer";
             btnAddCustomer.UseVisualStyleBackColor = true;
@@ -56,10 +56,9 @@
             // 
             // btnAddBook
             // 
-            btnAddBook.Location = new Point(10, 76);
-            btnAddBook.Margin = new Padding(3, 2, 3, 2);
+            btnAddBook.Location = new Point(11, 101);
             btnAddBook.Name = "btnAddBook";
-            btnAddBook.Size = new Size(156, 63);
+            btnAddBook.Size = new Size(178, 84);
             btnAddBook.TabIndex = 5;
             btnAddBook.Text = "Add Book";
             btnAddBook.UseVisualStyleBackColor = true;
@@ -67,10 +66,9 @@
             // 
             // btnRegisterBook
             // 
-            btnRegisterBook.Location = new Point(256, 180);
-            btnRegisterBook.Margin = new Padding(3, 2, 3, 2);
+            btnRegisterBook.Location = new Point(293, 240);
             btnRegisterBook.Name = "btnRegisterBook";
-            btnRegisterBook.Size = new Size(219, 63);
+            btnRegisterBook.Size = new Size(250, 84);
             btnRegisterBook.TabIndex = 3;
             btnRegisterBook.Text = "&Register Book";
             btnRegisterBook.UseVisualStyleBackColor = true;
@@ -79,56 +77,53 @@
             // cboCustomers
             // 
             cboCustomers.FormattingEnabled = true;
-            cboCustomers.Location = new Point(256, 9);
-            cboCustomers.Margin = new Padding(3, 2, 3, 2);
+            cboCustomers.Location = new Point(293, 12);
             cboCustomers.Name = "cboCustomers";
-            cboCustomers.Size = new Size(219, 23);
+            cboCustomers.Size = new Size(250, 28);
             cboCustomers.TabIndex = 0;
             cboCustomers.SelectedIndexChanged += cboCustomers_SelectedIndexChanged;
             // 
             // cboBooks
             // 
             cboBooks.FormattingEnabled = true;
-            cboBooks.Location = new Point(256, 60);
-            cboBooks.Margin = new Padding(3, 2, 3, 2);
+            cboBooks.Location = new Point(293, 80);
             cboBooks.Name = "cboBooks";
-            cboBooks.Size = new Size(219, 23);
+            cboBooks.Size = new Size(250, 28);
             cboBooks.TabIndex = 1;
             cboBooks.SelectedIndexChanged += cboBooks_SelectedIndexChanged;
             // 
             // dtpDate
             // 
-            dtpDate.Location = new Point(256, 106);
-            dtpDate.Margin = new Padding(3, 2, 3, 2);
+            dtpDate.Location = new Point(293, 141);
             dtpDate.Name = "dtpDate";
-            dtpDate.Size = new Size(219, 23);
+            dtpDate.Size = new Size(250, 27);
             dtpDate.TabIndex = 2;
             dtpDate.ValueChanged += dtpDate_ValueChanged;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(172, 15);
+            label1.Location = new Point(197, 20);
             label1.Name = "label1";
-            label1.Size = new Size(67, 15);
+            label1.Size = new Size(81, 20);
             label1.TabIndex = 6;
             label1.Text = "Customers:";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(197, 66);
+            label2.Location = new Point(225, 88);
             label2.Name = "label2";
-            label2.Size = new Size(42, 15);
+            label2.Size = new Size(52, 20);
             label2.TabIndex = 7;
             label2.Text = "Books:";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(204, 111);
+            label3.Location = new Point(233, 148);
             label3.Name = "label3";
-            label3.Size = new Size(34, 15);
+            label3.Size = new Size(44, 20);
             label3.TabIndex = 8;
             label3.Text = "Date:";
             // 
@@ -136,44 +131,55 @@
             // 
             lblCustomerError.AutoSize = true;
             lblCustomerError.ForeColor = Color.Red;
-            lblCustomerError.Location = new Point(480, 15);
+            lblCustomerError.Location = new Point(549, 20);
             lblCustomerError.Name = "lblCustomerError";
-            lblCustomerError.Size = new Size(0, 15);
+            lblCustomerError.Size = new Size(0, 20);
             lblCustomerError.TabIndex = 9;
             // 
             // lblBookError
             // 
             lblBookError.AutoSize = true;
             lblBookError.ForeColor = Color.Red;
-            lblBookError.Location = new Point(480, 66);
+            lblBookError.Location = new Point(549, 88);
             lblBookError.Name = "lblBookError";
-            lblBookError.Size = new Size(0, 15);
+            lblBookError.Size = new Size(0, 20);
             lblBookError.TabIndex = 10;
             // 
             // lblDateError
             // 
             lblDateError.AutoSize = true;
             lblDateError.ForeColor = Color.Red;
-            lblDateError.Location = new Point(480, 111);
+            lblDateError.Location = new Point(549, 148);
             lblDateError.Name = "lblDateError";
-            lblDateError.Size = new Size(0, 15);
+            lblDateError.Size = new Size(0, 20);
             lblDateError.TabIndex = 11;
             // 
             // lblErrorMsg
             // 
             lblErrorMsg.AutoSize = true;
             lblErrorMsg.ForeColor = Color.Red;
-            lblErrorMsg.Location = new Point(256, 147);
+            lblErrorMsg.Location = new Point(293, 196);
             lblErrorMsg.Name = "lblErrorMsg";
-            lblErrorMsg.Size = new Size(0, 15);
+            lblErrorMsg.Size = new Size(0, 20);
             lblErrorMsg.TabIndex = 12;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(12, 191);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(178, 84);
+            btnDelete.TabIndex = 13;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
             // 
             // RegisterBookForm
             // 
             AcceptButton = btnRegisterBook;
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(606, 272);
+            ClientSize = new Size(693, 363);
+            Controls.Add(btnDelete);
             Controls.Add(lblErrorMsg);
             Controls.Add(lblDateError);
             Controls.Add(lblBookError);
@@ -187,7 +193,6 @@
             Controls.Add(btnRegisterBook);
             Controls.Add(btnAddBook);
             Controls.Add(btnAddCustomer);
-            Margin = new Padding(3, 2, 3, 2);
             Name = "RegisterBookForm";
             Text = "Register Book";
             ResumeLayout(false);
@@ -209,5 +214,6 @@
         private Label lblBookError;
         private Label lblDateError;
         private Label lblErrorMsg;
+        private Button btnDelete;
     }
 }
