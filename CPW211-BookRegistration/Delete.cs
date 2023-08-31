644f72fb21d5d8a9d1695704e1e040675ebb4015
Reece Atkins <Reecepowered2@hotmail.com>
2023-08-30 17:50:02 -0700

    Created Delete onclick and basic isValid methods

--- CPW211-BookRegistration/Delete.cs
+++ CPW211-BookRegistration/Delete.cs
@@ -19,8 +19,58 @@ namespace CPW211_BookRegistration
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
+            if (IsValid())
+            {
+                ClearErrorMessages();
 
+                if (cboCustomer.SelectedIndex != -1)
+                {
+                    Customer customerToDelete = cboCustomer.SelectedItem as Customer;
+                    CustomerDB.Delete(customerToDelete);
+                    cboCustomer.SelectedIndex = -1;
+                    PopulateCustomerList();
+                }
+                else if (cboBook.SelectedIndex != -1)
+                {
+                    Book bookToDelete = cboBook.SelectedItem as Book;
+                    BookDB.Delete(bookToDelete);
+                    cboBook.SelectedIndex = -1;
+                    PopulateBookList();
+                }
+                else if (cboRegistration.SelectedIndex != -1)
+                {
+                    Registration regToDelete = cboRegistration.SelectedItem as Registration;
+                    BookRegistrationDB.Delete(regToDelete);
+                    cboRegistration.SelectedIndex = -1;
+                    PopulateRegistrationList();
+                }
+            }
         }
+
+        private bool IsValid()
+        {
+            bool isValid = true;
+            if (cboCustomer.SelectedIndex == -1 &&
+                cboBook.SelectedIndex == -1 &&
+                cboRegistration.SelectedIndex == -1) 
+            {
+                lblBookError.Text = "Please select a Book";
+                lblCustomerError.Text = "Please select a Customer";
+                lblRegistrationError.Text = "Please select a Registration";
+
+                isValid = false;
+            }
+
+            return isValid;
+        }
+
+        private void ClearErrorMessages()
+        {
+            lblBookError.Text = string.Empty;
+            lblCustomerError.Text = string.Empty;
+            lblRegistrationError.Text = string.Empty;
+        }
+
         public void PopulateCustomerList()
         {
             cboCustomer.Items.Clear();
